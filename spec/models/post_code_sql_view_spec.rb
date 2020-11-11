016e022855a7834f415c5779983a3af5a05d8070
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-10 20:08:18 -0700

    Fixes spacing in post code sql view spec

--- spec/models/post_code_sql_view_spec.rb
+++ spec/models/post_code_sql_view_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PostCodeData do 
   before(:each) do
-      @cafe1 = StreetCafe.create!(name: 'All Bar One', street_address: '27 East Parade', post_code: 'LS1 5BN', number_of_chairs: 20)
-      @cafe2 = StreetCafe.create!(name: 'All Bar Most Chairs', street_address: 'Unit D Electric Press, 4 Millenium Square', post_code: 'LS1 5BN', number_of_chairs: 140)
-      @cafe3 = StreetCafe.create!(name: 'Caffé Nero (Albion Place side)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 20)
-      @cafe4 = StreetCafe.create!(name: 'Caffé Nero (Albion Place side)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 16)
-      @cafe5 = StreetCafe.create!(name: 'Caffé Nero (MOST CHAIRS)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 22)
-      @cafe6 = StreetCafe.create!(name: 'BHS', street_address: '49 Boar Lane', post_code: 'LS1 5EL', number_of_chairs: 6)
-      @cafe7 = StreetCafe.create!(name: 'Hotel Chocolat', street_address: '55 Boar Lane', post_code: 'LS1 5EL', number_of_chairs: 12)
+    @cafe1 = StreetCafe.create!(name: 'All Bar One', street_address: '27 East Parade', post_code: 'LS1 5BN', number_of_chairs: 20)
+    @cafe2 = StreetCafe.create!(name: 'All Bar Most Chairs', street_address: 'Unit D Electric Press, 4 Millenium Square', post_code: 'LS1 5BN', number_of_chairs: 140)
+    @cafe3 = StreetCafe.create!(name: 'Caffé Nero (Albion Place side)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 20)
+    @cafe4 = StreetCafe.create!(name: 'Caffé Nero (Albion Place side)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 16)
+    @cafe5 = StreetCafe.create!(name: 'Caffé Nero (MOST CHAIRS)', street_address: '19 Albion Place', post_code: 'LS1 6JS', number_of_chairs: 22)
+    @cafe6 = StreetCafe.create!(name: 'BHS', street_address: '49 Boar Lane', post_code: 'LS1 5EL', number_of_chairs: 6)
+    @cafe7 = StreetCafe.create!(name: 'Hotel Chocolat', street_address: '55 Boar Lane', post_code: 'LS1 5EL', number_of_chairs: 12)
   end
 
   it 'returns all required data' do
